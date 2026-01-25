a21b0b710000a8de1a729fb35ed7e0e310057f3e
Praveen K Singh <pr4veensingh@proton.me>
2026-01-25 23:25:33 +0530

    Add in-memory room logic

--- backend/src/room.js
+++ backend/src/room.js
@@ -10,14 +10,10 @@ function joinRoom(roomId, socketId) {
 function leaveRoom(socketId) {
   for (const roomId in rooms) {
     rooms[roomId].delete(socketId);
-
     if (rooms[roomId].size === 0) {
-      delete rooms[roomId]; // auto destroy empty room
+      delete rooms[roomId];
     }
   }
 }
 
-module.exports = {
-  joinRoom,
-  leaveRoom,
-};
+module.exports = { joinRoom, leaveRoom };
