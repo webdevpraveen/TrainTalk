9135954837f9ce80bb280d91a78604814e11a20c
Praveen K Singh <pr4veensingh@proton.me>
2026-01-25 23:25:09 +0530

    Initialize Express and Socket.io server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,1 +1,21 @@
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
 
+const setupSocket = require("./socket");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: { origin: "*" }
+});
+
+setupSocket(io);
+
+server.listen(4000, () => {
+  console.log("🚆 RailChat backend running on port 4000");
+});
